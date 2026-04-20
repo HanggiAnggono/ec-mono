fb372bc99e900e64ef86d400c3c5ca3836ccf296
Hanggi Anggono <anggonohanggi@gmail.com>
2026-04-20 07:14:22 +0700

    Show login button in mobile settings

--- ec-mobile/src/screens/setting-screen.tsx
+++ ec-mobile/src/screens/setting-screen.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@/components/button'
 import Icon from '@/components/icon'
 import { Layout } from '@/layout/layout'
+import { Routes, StackScreenProp } from '@/screens'
 import { useUserGetProfile } from '@/shared/query/user/use-user-get-profile.query'
 import { useAuthStore } from '@/store/auth.store'
 import { useQueryClient } from '@tanstack/react-query'
 import { Alert, Pressable, Text, TouchableOpacity, View } from 'react-native'
 
-export const SettingScreen = () => {
-  const { data, isError, refetch } = useUserGetProfile()
+export const SettingScreen = ({
+  navigation,
+}: StackScreenProp<'Setting'>) => {
+  const { token, setAuthStore } = useAuthStore()
+  const { data, isError, refetch } = useUserGetProfile(undefined, {
+    enabled: !!token,
+  })
   const queryClient = useQueryClient()
-  const { setAuthStore } = useAuthStore()
 
   const handleLogout = () => {
     Alert.alert('Logout', 'Are you sure to logout?', [
@@ -30,7 +35,24 @@ export const SettingScreen = () => {
   return (
     <Layout className="flex-1 p-4">
       <View className="bg-background rounded-t-md p-4">
-        {isError ? (
+        {!token ? (
+          <View className="flex-row gap-3 items-center justify-between">
+            <View className="flex-row gap-3 flex-1">
+              <Icon
+                name="user"
+                size={25}
+                className="border border-text p-3 self-start color-text rounded-full"
+              />
+              <View className="flex-1 justify-center">
+                <Text className="text-lg text-text">You are not logged in</Text>
+                <Text className="text-text">Sign in to view your account.</Text>
+              </View>
+            </View>
+            <Button onPress={() => navigation.navigate(Routes.Login)}>
+              Login
+            </Button>
+          </View>
+        ) : isError ? (
           <Pressable
             onPress={() => {
               refetch()
@@ -54,13 +76,15 @@ export const SettingScreen = () => {
           </View>
         )}
       </View>
-      <TouchableOpacity
-        onPress={handleLogout}
-        className="items-center flex-row justify-between bg-background p-4 border-t border-t-background-400"
-      >
-        <Text className="text-text">Logout</Text>
-        <Icon className="color-text" name="right" size={16} />
-      </TouchableOpacity>
+      {token ? (
+        <TouchableOpacity
+          onPress={handleLogout}
+          className="items-center flex-row justify-between bg-background p-4 border-t border-t-background-400"
+        >
+          <Text className="text-text">Logout</Text>
+          <Icon className="color-text" name="right" size={16} />
+        </TouchableOpacity>
+      ) : null}
     </Layout>
   )
 }
